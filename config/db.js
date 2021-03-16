779dc4e6fded0edd6355052a236317efba2ca9d8
shagisayan <you@example.com>
2021-03-16 15:59:06 +0530

    first heroku file

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports={
+url :"mongodb://shagi_02:Shagi1997@mongodbproject-shard-00-00.f6he2.mongodb.net:27017,mongodbproject-shard-00-01.f6he2.mongodb.net:27017,mongodbproject-shard-00-02.f6he2.mongodb.net:27017/crud-app?ssl=true&replicaSet=atlas-dbu18c-shard-0&authSource=admin&retryWrites=true&w=majority"
+};
